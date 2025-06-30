8b52035b5855873b8e9c007019287a4f37721d1e
Yang Hang <yhsunshining@gmail.com>
2025-06-30 23:10:11 +0800

    AI Update files

---
+++ src/configs/routers.ts
@@ -0,0 +1,9 @@
+import SINGLEBUTTONPAGE from '../pages/SingleButtonPage.jsx';
+import HELLOWORLD from '../pages/HelloWorld.jsx';
+export const routers = [{
+  id: "SingleButtonPage",
+  component: SINGLEBUTTONPAGE
+}, {
+  id: "HelloWorld",
+  component: HELLOWORLD
+}]
